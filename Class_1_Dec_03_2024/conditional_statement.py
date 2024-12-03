3e5243640521d7a5022a2d3be228a4ae6c669128
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-12-03 18:43:31 +0100

    Feat: Initial Commit

---
+++ Class_1_Dec_03_2024/conditional_statement.py
@@ -0,0 +1,10 @@
+numbers = 4
+
+if numbers < 4:
+    print("I am greater than what you think I am")
+
+elif numbers == 3:
+    print("You are still wrong")
+
+else: 
+    print("You are correct")
---
+++ Class_1_Dec_03_2024/first_class.py
@@ -0,0 +1,27 @@
+# Dictionaries /Hash Maps
+class_dict = {"name":"bag", "number":2, "color": {"black":1, "blue": [1,2,3], "red":2}, "vendor": "shopify"}
+
+# Tuples
+# immutable list, a list that cannot be changed later
+class_list = ("ezekiel", "gideon", "henry")
+
+# List
+# mutable list, a list that can be changed later
+class_names = ["babatunde", "beatrice", "chioma", 1, 2, 3.5 ]
+
+# Integers
+number = 345
+
+# Floats
+currency = 3.5
+
+print(number + currency)
+print(number - currency)
+print(number * currency)
+print(number / currency)
+
+# Boolean
+is_black = True
+
+
+
