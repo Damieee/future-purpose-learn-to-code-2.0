4d009341a2440dbf629ffa7997fbc41d91518621
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-12-23 13:09:28 +0100

    feat: update BookRequest model to include category and improve field titles

--- Class_2_FastAPI_DEC_23_2024/validators.py
+++ Class_2_FastAPI_DEC_23_2024/validators.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel, Field
 
 class BookRequest(BaseModel):  
-    id: int
-    title: str = Field(min_length=3)
-    author: str = Field(min_length=1)
-    description: str = Field(min_length=1, max_length=100)
-    rating: int = Field(gt=0, lt=5)
+    title: str = Field(min_length=3, title="Title of the book")
+    author: str = Field(min_length=3, title="Author of the book")
+    category: str = Field(min_length=3, title="Category of the book")
